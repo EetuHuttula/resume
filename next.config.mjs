0bcc6070304ad1c2aa90395e700108d67c31e94e
eetu <eetuhuttula99@gmail.com>
2025-10-31 23:36:22 +0200

    ff

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  // Korjaa basePath vastaamaan repositorion nimeä
-  basePath: '/resume', 
-  // Korjaa assetPrefix vastaamaan repositorion nimeä
-  assetPrefix: '/resume/',
   images: {
     unoptimized: true,
   },
